9ef701112a698d178bce66ce8e4ef4bb5d45efd3
FOS-Intern <intern@fata-organa.com>
2024-03-09 04:42:07 +0700

    add basic greedy logic

---
+++ src/GameEngine/game/logic/savage.py
@@ -0,0 +1,107 @@
+import random
+from typing import Optional
+
+from game.logic.base import BaseLogic
+from game.models import GameObject, Board, Position
+from ..util import get_direction
+from collections import deque
+
+MIN_GLOBAL_VALUE = -29
+MAX_GLOBAL_VALUE = 29
+
+
+class SavageLogic(BaseLogic):
+    def __init__(self):
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.goal_position: Optional[Position] = None
+        self.current_direction = 0
+
+    def move_position(self, current_position: Position, delta_x: int, delta_y: int):
+        return Position(current_position.x + delta_x, current_position.y + delta_y)
+    
+    def is_coordinate_equal(self, a: Position, b: Position):
+        return a.x == b.x and a.y == b.y
+
+    def next_move(self, board_bot: GameObject, board: Board):
+        value_move = -1
+        temp_teleport_pos = []
+        temp_diamond_button_pos = deque()
+        temp_diamonds1_pos = deque()
+        temp_diamonds2_pos = deque()
+        temp_enemy = deque()
+        base_game_pos = None
+        current_position = board_bot.position
+        current_id = board_bot.id
+        current_score_inven = board_bot.properties.diamonds
+        max_global_value = current_score_inven
+        max_val_dirr = [current_score_inven, current_score_inven, current_score_inven, current_score_inven]
+
+        # Store each component items in the board to decrease the time complexity checking
+        for i in board.game_objects:
+            if(i.type == "TeleportGameObject"):
+                temp_teleport_pos.append(i.position)
+            elif(i.type == "DiamondButtonGameObject"):
+                temp_diamond_button_pos.append(i.position)
+            elif(i.type == "DiamondGameObject"):
+                if(i.properties.points == 1):
+                    temp_diamonds1_pos.append(i.position)
+                else:
+                    temp_diamonds2_pos.append(i.position)
+            elif(i.type == "BaseGameObject"):
+                base_game_pos = i.position
+            elif(i.type == "BotGameObject"):
+                if(i.id != current_id):
+                    temp_enemy.append(i)
+        is_move = [False, False, False, False]
+
+        while len(temp_diamonds1_pos) != 0:
+            diamond_pos = temp_diamonds1_pos.pop()
+            for i in range(4):
+                next_pos = self.move_position(current_position, self.directions[i][0], self.directions[i][1])
+                if(self.is_coordinate_equal(next_pos, diamond_pos)):
+                    is_move[i] = True
+                    max_val_dirr[i] += 1
+
+        while len(temp_diamonds2_pos) != 0:
+            diamond_pos = temp_diamonds2_pos.pop()
+            for i in range(4):
+                next_pos = self.move_position(current_position, self.directions[i][0], self.directions[i][1])
+                if(self.is_coordinate_equal(next_pos, diamond_pos)):
+                    is_move[i] = True
+                    max_val_dirr[i] += 2
+
+        while len(temp_enemy) != 0:
+            enemy_pos = temp_enemy.pop()
+            for i in range(4):
+                next_pos = self.move_position(current_position, self.directions[i][0], self.directions[i][1])
+                if(self.is_coordinate_equal(next_pos, enemy_pos.position)):
+                    is_move[i] = True
+                    max_val_dirr[i] += enemy_pos.properties.diamonds
+        
+        for i in range(4):
+            if(max_val_dirr[i] > max_global_value):
+                max_global_value = max_val_dirr[i]
+                value_move = i
+
+        if(max_global_value == current_score_inven):
+            for i in range(4):
+                if(current_position.x+self.directions[i][0] == base_game_pos.x and current_position.y+self.directions[i][1] == base_game_pos.y):
+                    value_move = i
+                    break
+        
+        delta_x = self.directions[value_move][0]
+        delta_y = self.directions[value_move][1]
+
+        props = board_bot.properties
+        # Analyze new state
+        if props.diamonds == 5:
+            # Move to base
+            base = board_bot.properties.base
+            self.goal_position = base
+            delta_x, delta_y = get_direction(
+                current_position.x,
+                current_position.y,
+                self.goal_position.x,
+                self.goal_position.y,
+            )
+        return delta_x, delta_y
---
+++ src/GameEngine/run-test-bots.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
+python3 main.py --logic Savage --email=test1@email.com --name=stima1 --password=123456 --team etimo &
